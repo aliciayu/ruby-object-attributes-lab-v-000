aa087341b04f39a3e890089ae5f356b074e92ac1
Alicia Yu <alicia.yu.16@gmail.com>
2018-05-16 21:39:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,14 @@ class Dog
   end
 
   def name
-    @name
+    @this_dogs_name
   end
 
   def breed=(dogs_breed)
-    @breed=dogs_breed
+    @this_dogs_breed=dogs_breed
   end
 
   def breed
-    @breed
+    @this_dogs_breed
   end
 end
